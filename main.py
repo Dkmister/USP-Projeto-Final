2b442d76d64fc06065170ec0bb098813b118c03a
Vil Aprato <vilmar.neto@gmail.com>
2024-11-04 23:25:01 -0300

    Update main.py
    Functional

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 class Modelo():
     def __init__(self):
-        pass
+        self = self
 
     def CarregarDataset(self, path):
         """
@@ -17,7 +22,11 @@ class Modelo():
         O dataset é carregado com as seguintes colunas: SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm e Species.
         """
         names = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species']
-        self.df = pd.read_csv(path, names=names)
+        try:
+          self.df = pd.read_csv(path, names=names)
+          print("Dataset carregado com sucesso.\n")
+        except FileNotFoundError:
+          print("Arquivo não encontrado. Verifique o caminho especificado.\n")
 
     def TratamentoDeDados(self):
         """
@@ -32,7 +41,16 @@ class Modelo():
             * Explore gráficos e visualizações para obter insights sobre a distribuição dos dados.
             * Certifique-se de que os dados estão limpos e prontos para serem usados no treinamento do modelo.
         """
-        pass
+        # print(self.df.isnull().sum().sum()) # Sem valores NaN
+        
+        # Transformando os valores em rótulos de 0 a 2
+        encoder = LabelEncoder()
+        obj_cols = ['Species']
+
+        values = self.df[obj_cols[0]].values
+        encoder.fit(values)
+        self.df[obj_cols[0]] = encoder.transform(values)
+       
 
     def Treinamento(self):
         """
@@ -45,7 +63,11 @@ class Modelo():
         
         Nota: Esta função deve ser ajustada conforme o modelo escolhido.
         """
-        pass
+        self.X=self.df.drop('Species', axis=1)  # Features (todas as colunas exceto a de alvo)
+        self.y=self.df['Species']
+
+        self.X_train,self.X_test,self.y_train,self.y_test=train_test_split(self.X,self.y,test_size=0.2)
+        
 
     def Teste(self):
         """
@@ -54,7 +76,34 @@ class Modelo():
         Esta função deve ser implementada para testar o modelo e calcular métricas de avaliação relevantes, 
         como acurácia, precisão, ou outras métricas apropriadas ao tipo de problema.
         """
-        pass
+        logreg=LogisticRegression()
+        logreg.fit(self.X_train,self.y_train)
+
+        
+        y_pred=logreg.predict(self.X_test)
+        print('Precisão de Regressão Logistica:')
+        print(metrics.accuracy_score(self.y_test,y_pred))
+        print('Score:')
+        print(logreg.score(self.X, self.y))
+
+        lreg=HistGradientBoostingClassifier()
+        lreg.fit(self.X_train,self.y_train)
+
+        
+        y_pred=lreg.predict(self.X_test)
+        print('Precisão de Gradiente Booster:')
+        print(metrics.accuracy_score(self.y_test,y_pred))
+        print('Score:')
+        print(lreg.score(self.X, self.y))
+
+        knn=KNeighborsClassifier(n_neighbors=5)
+        knn.fit(self.X_train,self.y_train)
+        
+        y_pred=knn.predict(self.X_test)
+        print('Precisao do KNN com 5 vizinhos')
+        print(metrics.accuracy_score(self.y_test,y_pred))
+        print('Score:')
+        print(knn.score(self.X, self.y))
 
     def Train(self):
         """
@@ -73,6 +122,8 @@ class Modelo():
         self.TratamentoDeDados()
 
         self.Treinamento()  # Executa o treinamento do modelo
+
+        self.Teste()
 
 # Lembre-se de instanciar as classes após definir suas funcionalidades
 # Recomenda-se criar ao menos dois modelos (e.g., Regressão Linear e SVM) para comparar o desempenho.
